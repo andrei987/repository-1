202ffe5a2357f8e6ee7034dee64a6e822d876646
U-P6109\andrei.obreja <P6109+andrei.obreja@P6109.svdomain1.softvision.ro>
2016-03-31 12:39:30 +0300

    ultratest3

---
+++ ultratest3_hostname.py
@@ -0,0 +1,29 @@
+#! python
+
+import socket # for socket
+import sys 
+
+list = ['www.google.ro' , 'www.yahoo.com' , 'https://github.com/andrei987/repository-1/blob/master/ultratest2_server.py']
+port = 80
+
+for i in list:
+    try:
+    	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    	print "Socket successfully created"
+    except socket.error as err:
+    	print "socket creation failed with error %s" %(err)
+
+
+    try:
+        host_ip = socket.gethostbyname(i)
+
+    except socket.gaierror:
+    #this means could not resolve the host
+    	print "there was an error resolving the host", i
+    	sys.exit()
+
+    # connecting to the server
+    s.connect((host_ip,port))
+
+    print "the socket has successfully connected to",  i, " on port == %s" %(host_ip)
+    
